def92b147b86a7539546d5be907c03e2534e89af
hanyuntao <hanyuntaocn@163.com>
2017-04-13 14:04:04 +0800

    提交

--- 图片库/简单图片库/script/showPic.js
+++ 图片库/简单图片库/script/showPic.js
@@ -1,8 +1,8 @@
 function showPic(whichpic) {
 	var source=whichpic.getAttribute("href");
-	var txt=whichPic.getAttribute("title");
+	var text=whichpic.getAttribute("title");
 	var placeholder=document.getElementById("placeholder");
 	var picTitle=document.getElementById("picTitle");
 	placeholder.setAttribute("src",source);
-	picTitle.innerHTML=txt;
+	picTitle.firstChild.nodeValue=text;
 }
