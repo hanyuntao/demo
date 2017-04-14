197026a590835dbefc4516b5841c97077fada9c6
hanyuntao <hanyuntaocn@163.com>
2017-04-14 09:25:15 +0800

    添加自动播放动画

--- 焦点轮播图/switch.js
+++ 焦点轮播图/switch.js
@@ -4,10 +4,17 @@ window.onload=function() {
 	var buttons = document.getElementById("buttons").getElementsByTagName("span");
 	var prev = document.getElementById("prev");
 	var next = document.getElementById("next");
-	var index=0;				//用于索引当前按钮
+	var index=0;			//用于索引当前按钮
+	var len=5;				//当前图片的数量
+	var animated=false;		//用于判断切换是否进行
+	var interval=3000;		//自动播放定时器秒数，这里是3秒
+	var timer;				//定时器
 
 	// 左右箭头点击事件
 	prev.onclick=function() {
+		if (animated) {    //如果切换还在进行，则直接结束，直到切换完成
+            return;
+        }
 		if (index==0) {
 			index=4;
 		}
@@ -19,6 +26,9 @@ window.onload=function() {
 
 	}
 	next.onclick=function() {
+		if (animated) {    //如果切换还在进行，则直接结束，直到切换完成
+            return;
+        }
 		if (index==4) {
 			index=0;
 		}
@@ -32,6 +42,9 @@ window.onload=function() {
 	// 按钮点击事件
 	for (var i = 0; i < buttons.length; i++) {
 		buttons[i].onclick=function() {
+			if (animated) {   //如果切换还在进行，则直接结束，直到切换完成
+                return;
+            }
 			//当继续点击当前按钮的时候，不进行切换
 			if(this.className == 'on') {
                 return;
@@ -45,18 +58,48 @@ window.onload=function() {
 		}
 	}
 
+	container.onmouseover = stop;//父容器的移入移出事件
+	container.onmouseout = play;
+
+	play();  //调用自动播放函数
 
 	// 切换动画
+	/* 改进切换动画
+	主要实现方法是通过定时器setTimeout。
+	1. 设置切换需要的总时间，每次间隔多少时间，然后求出每次切换过渡的位移量，
+	2. 判断是否到达目标值，若否，则继续执行定时器进行位移。
+	3. 如果连续点击切换按钮，图片会立刻不停地切换，
+	   但我们想要的效果是等当前图片切换完成之后再进行下一次切换，这个可以优化一下。
+	4. 最后实现自动播放效果，当鼠标不点击时，它能自动播放，
+	 这里用到setInterval定时器，每次3秒执行一次点击事件，
+	 而当鼠标移上去的时候清除该事件，离开的时候又自动播放。*/
 	function animate(offset) {
+		animated = true;	 //切换进行中
+		var time = 300;     //位移总时间
+		var interval = 10;	//位移间隔时间，间隔时间越短，动画越流畅。
+		var speed = offset/(time/interval);//每次位移量
 		// 必须要用offsetLeft
 		// style.left的值需要事先定义，否则取到的值为空，而且必须要定义在html里。
-		piclist.style.left=piclist.offsetLeft+offset+"px";
-		if(piclist.offsetLeft>0) {
-			piclist.style.left=-2400+"px";
+		var left = piclist.offsetLeft+offset;
+		// piclist.style.left=piclist.offsetLeft+offset+"px";
+		var go = function() {
+			//这两种情况表示还在切换中
+			if((speed > 0 && piclist.offsetLeft < left) || (speed < 0 && piclist.offsetLeft > left)) {
+				piclist.style.left = piclist.offsetLeft + speed + "px";
+				setTimeout(go,interval);	//继续执行切换go()函数
+			}
+			else {
+				piclist.style.left = left + "px";
+				if(left>0) {
+					piclist.style.left=-2400+"px";
+				}
+				if (left<-2400) {
+					piclist.style.left="0px";
+				}
+				animated = false;    //切换完成
+			}
 		}
-		if (piclist.offsetLeft<-2400) {
-			piclist.style.left=0+"px";
-		}
+		go();
 	}
 
 	// 用于为按钮添加样式
@@ -70,4 +113,16 @@ window.onload=function() {
 		 //为当前按钮添加类
 		buttons[index].className="on";
 	}
+
+	//自动播放
+    function play() {
+        timer = setTimeout(function () {
+            next.onclick();
+            play();
+        }, interval);
+    }
+     //清除定时器
+    function stop() {
+        clearTimeout(timer);
+    }
 }
